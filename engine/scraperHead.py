4145d2da40d0ae54b1485c1891b3e7be9feb1215
elrumiiii@outlook.com <elrumiiii@outlook.com>
2021-03-25 12:02:03 +0700

    change headcount algorith to --headless

--- engine/scraperHead.py
+++ engine/scraperHead.py
@@ -9,7 +9,23 @@ from selenium.webdriver.chrome.options import Options
 def hc(value):
     head = None
     try:
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        time.sleep(random.randint(1, 2))
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        options.add_argument(f'user-agent={user_agent}')
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--allow-running-insecure-content')
+        options.add_argument("--disable-extensions")
+        options.add_argument("--proxy-server='direct://'")
+        options.add_argument("--proxy-bypass-list=*")
+        options.add_argument("--start-maximized")
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        driver = webdriver.Chrome(
+            ChromeDriverManager().install(), options=options)
         url = f'http://www.linkedin.com/company/{value}/'
         driver.maximize_window()
         driver.get(url)
@@ -29,6 +45,6 @@ def hc(value):
             head = 0.35
     except:
         driver.quit()
-        head = 0
+        head = 0.175
 
     return head

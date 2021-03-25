57965cc69bbbd402fe82b78d367c5bda6dda689e
elrumiiii@outlook.com <elrumiiii@outlook.com>
2021-03-25 12:27:34 +0700

    Debug scraper engine

--- engine/scraperHead.py
+++ engine/scraperHead.py
@@ -3,6 +3,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
 import random
+import os
 from selenium.webdriver.chrome.options import Options
 
 
@@ -12,22 +13,14 @@ def hc(value):
         time.sleep(random.randint(1, 2))
         user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
         options = webdriver.ChromeOptions()
-        options.headless = True
+        options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        options.add_argument("--headless")
         options.add_argument(f'user-agent={user_agent}')
-        options.add_argument("--window-size=1920,1080")
-        options.add_argument('--ignore-certificate-errors')
-        options.add_argument('--allow-running-insecure-content')
-        options.add_argument("--disable-extensions")
-        options.add_argument("--proxy-server='direct://'")
-        options.add_argument("--proxy-bypass-list=*")
-        options.add_argument("--start-maximized")
-        options.add_argument('--disable-gpu')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--no-sandbox')
-        driver = webdriver.Chrome(
-            ChromeDriverManager().install(), options=options)
+        driver = webdriver.Chrome(executable_path=os.environ.get(
+            "CHROMEDRIVER_PATH"), options=options)
         url = f'http://www.linkedin.com/company/{value}/'
-        driver.maximize_window()
         driver.get(url)
         content = driver.page_source.encode('utf-8').strip()
         soup = BeautifulSoup(content, "html.parser")
